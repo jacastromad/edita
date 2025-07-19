0b8682cad4a2880e516d6f9166e7f30428000e3a
Jose Antonio Castro <jacastromad@gmail.com>
2025-07-19 23:30:16 +0200

    Target SDK (35)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,14 +5,14 @@ plugins {
 
 android {
     namespace = "com.github.jacastromad.edita"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.github.jacastromad.edita"
         minSdk = 29
-        targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        targetSdk = 35
+        versionCode = 3
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
